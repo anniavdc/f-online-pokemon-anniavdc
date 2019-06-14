6fe9273828ea20932e9533093ab9b9456a526611
Annia da Costa <annia.vdc@gmail.com>
2019-06-15 00:12:03 +0200

    Create main components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Pokemon from './components/Pokemon';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:[],
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/pokemon' component={ Pokemon } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
