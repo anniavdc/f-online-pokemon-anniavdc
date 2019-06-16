a4eebf6bf12064fcc0f4b3e050cc498c0da3f23e
Annia da Costa <annia.vdc@gmail.com>
2019-06-16 14:42:47 +0200

    Create Pokelist method with regular expresions to render each image without another fetch

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-class PokeCard extends React.Component{
-    render(){
-        const {name, url} = this.props;
-        return(
-            <p>{name}</p>
+class PokeCard extends React.Component {
+    render() {
+        const { name, image } = this.props;
+        console.log(image);
+        
+        return (
+            <div>
+                <img className='PokeCard__image' src={image} alt={name}/>
+                <h2 className='PokeCard__title'>{name}</h2>
+            </div>
         )
     }
 }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,6 +2,20 @@ import React from 'react';
 import PokeCard from './PokeCard';
 
 class PokeList extends React.Component {
+    constructor(props){
+        super(props);
+        this.getImageSrc=this.getImageSrc.bind(this);
+    }
+    getImageSrc(url){
+        const regex = /\/(\d+)\//g;
+        const string = url;
+        const matches = regex.exec(string);
+        console.log(matches);
+        const id = matches[1];
+        const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        
+        return src;
+    }
     render() {
         const { dataList } = this.props;
         return (
@@ -10,7 +24,7 @@ class PokeList extends React.Component {
                     <li>
                         <PokeCard 
                         name={pokemon.name}
-                        image={pokemon.url}/>
+                        image={this.getImageSrc(pokemon.url)}/>
                     </li>
                 )}
             </ul>
